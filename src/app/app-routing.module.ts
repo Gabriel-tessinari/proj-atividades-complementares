829d7994381b9f2cf0a22e4d2397484b4c5b0d3f
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-02-26 23:55:22 -0300

    Primeiro componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CursosModule } from './pages/cursos/cursos.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/cursos/cursos.module').then(m => m.CursosModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

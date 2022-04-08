97d882221b8cfeb4e29563059c657057d6f785f9
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-04-07 23:17:23 -0300

    ID do curso selecionado

--- src/app/pages/adm/certificados/certificados.component.ts
+++ src/app/pages/adm/certificados/certificados.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CursoJson } from 'src/app/shared/json/curso.json';
 import { HorasComplementaresJson } from 'src/app/shared/json/horas-complementares.json';
-import { PontuacaoJson } from 'src/app/shared/json/pontuacao.json';
 import { TesteArqService } from 'src/app/shared/services/testeArq.service';
 
 @Component({
@@ -11,8 +10,8 @@ import { TesteArqService } from 'src/app/shared/services/testeArq.service';
 })
 export class CertificadosComponent implements OnInit {
   cursos: Array<CursoJson>;
-  horasComplementares: Array<HorasComplementaresJson>;
   idCursoSelecionado: number;
+  horasComplementares: Array<HorasComplementaresJson>;
   alunos: Array<Aluno>;
   
   constructor( 
@@ -21,8 +20,9 @@ export class CertificadosComponent implements OnInit {
 
   ngOnInit(): void {
     this.cursos = [];
-    this.loadCursos();
+    this.idCursoSelecionado = 0;
     this.alunos = [];
+    this.loadCursos();
   }
 
   loadCursos() {   
@@ -37,7 +37,19 @@ export class CertificadosComponent implements OnInit {
     );
   }
 
+  selectCurso(selected: string) {
+    for(let i = 0; i < this.cursos.length; i++) {
+      if(selected == this.cursos[i].name) {
+        this.idCursoSelecionado = this.cursos[i].id;
+        i = this.cursos.length;
+      } else {
+        this.idCursoSelecionado = 0;
+      }
+    }
+  }
+
   loadHorasComplementares() {
+    console.log(this.idCursoSelecionado)
     this.testeArqService.getHorasComplementaresByCurso(this.idCursoSelecionado)
     .subscribe(
       response => {

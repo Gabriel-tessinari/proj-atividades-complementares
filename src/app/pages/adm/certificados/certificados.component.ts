3c1c1ee181033081de37154623f6d4444c1a993d
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-04-08 21:01:35 -0300

    Ajustando tela certificados

--- src/app/pages/adm/certificados/certificados.component.ts
+++ src/app/pages/adm/certificados/certificados.component.ts
@@ -49,7 +49,6 @@ export class CertificadosComponent implements OnInit {
   }
 
   loadHorasComplementares() {
-    console.log(this.idCursoSelecionado)
     this.testeArqService.getHorasComplementaresByCurso(this.idCursoSelecionado)
     .subscribe(
       response => {
@@ -62,7 +61,9 @@ export class CertificadosComponent implements OnInit {
     );
   }
 
-  loadAlunos() {   
+  loadAlunos() {
+    this.alunos = [];
+
     this.horasComplementares.forEach(hc=>{
       let alunoNaLista: boolean = false;
       this.alunos.forEach(aluno =>{
@@ -84,14 +85,68 @@ export class CertificadosComponent implements OnInit {
       }     
     });
   }
+
+  sortTable(column: number) {
+    let alunosAux: Array<Aluno> = [];
+
+    
+  }
+
+  /*customSort(event: SortEvent) {
+    event.data.sort((data1, data2) => {
+      let value1;
+      let value2;
+      switch (event.field) {
+        case "origin.productionOrder.product.code":
+          value1 = data1.origin.productionOrder.product.code;
+          value2 = data2.origin.productionOrder.product.code;
+          break;
+        case "origin.productionOrder.product.derivation.code":
+          value1 = data1.origin.productionOrder.product.derivation.code;
+          value2 = data2.origin.productionOrder.product.derivation.code;
+          break;
+        case "origin.productionOrder.opNumber":
+          value1 = data1.origin.productionOrder.opNumber;
+          value2 = data2.origin.productionOrder.opNumber;
+          break;
+        case "quality.code":
+          value1 = data1.quality.code;
+          value2 = data2.quality.code;
+          break;
+        default:
+          value1 = data1[event.field];
+          value2 = data2[event.field];
+          break;
+      }
+      let result = null;
+
+      if (value1 == null && value2 != null) {
+        result = -1;
+      }
+      else if (value1 != null && value2 == null) {
+        result = 1;
+      }
+      else if (value1 == null && value2 == null) {
+        result = 0;
+      }
+      else if (typeof value1 === 'string' && typeof value2 === 'string') {
+        result = value1.localeCompare(value2);
+      }
+      else {
+        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+      }
+      return (event.order * result);
+    });
+    this.checkOrderMovement(this.movementChanged, true);
+  }*/
 }
 
-class Aluno{
+class Aluno {
   nome: string;
   matricula: number;
   certificadosPendentes: number;
 
-  constructor(){
+  constructor() {
     this.nome = '';
     this.matricula = 0;
     this.certificadosPendentes = 0;

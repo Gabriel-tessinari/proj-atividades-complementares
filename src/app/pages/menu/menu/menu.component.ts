260c173657bfbd8a91eecf2000003758b81b5991
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-04-15 11:22:48 -0300

    Detalhe

--- src/app/pages/menu/menu/menu.component.ts
+++ src/app/pages/menu/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cursos',
+  selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })

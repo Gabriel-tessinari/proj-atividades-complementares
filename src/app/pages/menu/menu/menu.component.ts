0ef5804406f0ae032b24b98298a60a539faeface
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-04-15 00:48:03 -0300

    Menu teste para ser tela com url vazia

---
+++ src/app/pages/menu/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}

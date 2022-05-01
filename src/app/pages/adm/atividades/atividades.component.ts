c1bd3ad224c89dfdec42325e0e8dc757e155f396
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-05-01 03:12:25 -0300

    atividades: load grupos de atividades

--- src/app/pages/adm/atividades/atividades.component.ts
+++ src/app/pages/adm/atividades/atividades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TesteArqService } from 'src/app/shared/services/testeArq.service';
+import { GrupoAtividadesJson } from 'src/app/shared/json/grupo-atividades.json';
 
 @Component({
   selector: 'app-atividades',
@@ -6,10 +8,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atividades.component.scss']
 })
 export class AtividadesComponent implements OnInit {
+  grupos: Array<GrupoAtividadesJson>;
+  idGrupoSelecionado: number;
+  desabilitaButton: boolean;
+  alertas: Alertas;
 
-  constructor() { }
+  constructor(
+    private testeArqService: TesteArqService
+  ) { }
 
   ngOnInit(): void {
+    this.grupos = [];
+    this.idGrupoSelecionado = 0;
+    this.desabilitaButton = true;
+    this.alertas = new Alertas();
+    this.loadGrupos();
   }
 
+  loadGrupos() {   
+    this.testeArqService.getGruposAtividades()
+    .subscribe(
+      response => {
+        this.grupos = response
+      },
+      error => {
+        console.log(error);
+        this.alertas.grupo = true;
+      }
+    );
+  }
+
+  selectGrupo(selected: string) {
+    for(let i = 0; i < this.grupos.length; i++) {
+      if(selected == this.grupos[i].nome) {
+        this.idGrupoSelecionado = this.grupos[i].id;
+        i = this.grupos.length;
+      } else {
+        this.idGrupoSelecionado = 0;
+      }
+    }
+
+    this.desabilitaButton = (this.idGrupoSelecionado == 0);
+  }
+}
+
+
+class Alertas {
+  grupo: boolean;
+ 
+  grupoMensagem: string;
+
+  constructor() {
+    this.grupo = false;
+    this.grupoMensagem = 'Erro ao carregar a lista de grupos de atividades.';
+  }
 }

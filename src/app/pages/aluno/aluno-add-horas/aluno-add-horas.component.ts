f0deea2932621b398aefec273bffcb97982e2ae9
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-06-01 18:09:09 -0300

    aluno/add-horas: visual completo, apenas get

--- src/app/pages/aluno/aluno-add-horas/aluno-add-horas.component.ts
+++ src/app/pages/aluno/aluno-add-horas/aluno-add-horas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AtividadeJson } from 'src/app/shared/json/atividade.json';
+import { GrupoAtividadesJson } from 'src/app/shared/json/grupo-atividades.json';
+import { TesteArqService } from 'src/app/shared/services/testeArq.service';
 
 @Component({
   selector: 'app-aluno-add-horas',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aluno-add-horas.component.scss']
 })
 export class AlunoAddHorasComponent implements OnInit {
+  grupos: Array<GrupoAtividadesJson>;
+  idGrupoSelecionado: number;
+  atividades: Array<AtividadeJson>;
+  idAtividadeSelecionada: number;
 
-  constructor() { }
+  constructor(
+    private service: TesteArqService
+  ) { }
 
   ngOnInit(): void {
+    this.grupos = [];
+    this.idGrupoSelecionado = 0;
+    this.atividades = [];
+    this.idAtividadeSelecionada = 0;
+    this.loadGrupos();
   }
 
+  loadGrupos() {
+    this.service.getGruposAtividades()
+    .subscribe(
+      response => {
+        this.grupos = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadAtividades() {
+    this.service.getAtividades()
+    .subscribe(
+      response => {
+        this.setAtividadesFromGrupo(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  setAtividadesFromGrupo(atividades: Array<AtividadeJson>) {
+    this.atividades = [];
+    this.idAtividadeSelecionada = 0;
+
+    atividades.forEach(atv => {
+      if(atv.grupoAtividadesId == this.idGrupoSelecionado) {
+        this.atividades.push(atv);
+      }
+    })
+  }
 }
